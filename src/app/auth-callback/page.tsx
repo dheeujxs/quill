58117894a72a1f5423d763f7f0dbf39c54ed2d5f
dheeujjxs <dheerajgoswami@gmail.com>
2024-07-17 13:22:07 +0530

    landing page and authentication done

---
+++ src/app/auth-callback/page.tsx
@@ -0,0 +1,25 @@
+import { useRouter, useSearchParams } from "next/navigation"
+import { trpc } from "../_trpc/client";
+
+const Page = async () => {
+    const router  = useRouter()
+
+    const  searchParams = useSearchParams();
+    const origin = searchParams.get('origin')
+
+        const apiResponse = await fetch('/api/whatever')
+        
+
+
+
+        const data = await  apiResponse.json()
+
+
+
+
+
+
+
+}
+
+export default Page
